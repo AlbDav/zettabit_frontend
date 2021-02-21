e68a385399076eaa8fb29c81ebf1d40ab124b5f2
Davide Albano <davidealbano96@gmail.com>
2021-02-21 21:04:54 +0100

    prosemirror-to-vue - started article render

---
+++ src/components/Article/ArticleRender.vue
@@ -0,0 +1,24 @@
+<template>
+    <article-node :type="node.type" :marks="node.marks">
+        <template v-if="node.content">
+            <article-render v-for="(subNode, i) in node.content" :key="i" :node="subNode">
+            </article-render>
+        </template>
+        <template v-else>
+            {{ node.text }}
+        </template>
+    </article-node>
+</template>
+
+<script>
+import ArticleNode from '../components/Article/ArticleNode.vue';
+import ArticleNode from './ArticleNode.vue';
+
+export default {
+    name: 'article-render',
+    props: ['node'],
+    components: {
+        ArticleNode
+    }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -4,3 +4,14 @@
     <input type="text" class="sul-text-field" placeholder="Text">
   </div>
 </template>
+
+<script>
+import ArticleRender from '../components/Article/ArticleRender,vue';
+import ArticleNode from '../components/Article/ArticleNode.vue';
+
+export default {
+  data() {
+    return {}
+  }
+}
+</script>

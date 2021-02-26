25b62b6b3ce7999d553333f90f8a638399f011ad
Davide Albano <davidealbano96@gmail.com>
2021-02-26 19:02:17 +0100

    basic-prosemirror-setup - installed all prosemirror packges

---
+++ src/components/Editor/ArticleEditor.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="editor"></div>
+</template>
+
+<script>
+import { schema } from "prosemirror-schema-basic";
+import { EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+
+export default {
+	mounted() {
+		this.initializeEditor();
+	},
+	methods: {
+		inizializeEditor() {
+			let state = EditorState.create({ schema });
+			let view = new EditorView(document.querySelector('#editor'), { state });
+		}
+	}
+}
+</script>
